093d04f2a3c2320fa3b19d71a65fa92f12c3484d
ikram-Sakura <123576941+ikram-Sakura@users.noreply.github.com>
2023-04-25 23:37:09 +0000

    Update main.h

--- main.h
+++ main.h
@@ -9,14 +9,14 @@
 
 int _printf(const char *format, ...);
 
-int	ft_putchar(char c);
-int	ft_putnbr(long int n);
-int	ft_putstr(char *str, int cheack);
-int	ft_unsignedint(unsigned int n);
-int	ft_hexadecimal(unsigned int n, char *base);
+int	string_conversion(char c);
+int	ft_pointer(long int n);
+int	flag_conversion(char *str, int cheack);
+int	length_modifiers(unsigned int n);
+int	specifier_conversion(unsigned int n, char *base);
 int	ft_adresse(unsigned long int n, char *base);
-int ft_octal(unsigned int n);
-int ft_binary(unsigned int n);
+int buffered_printf(unsigned int n);
+int binary_conversion(unsigned int n);
 int rot13(char *str);
 int rev_str(char *str);
 
